5f82b40cda200c3770622479f1b6eae9f477548f
Agustín Firpo <agustin_firpoutlook.com.ar>
2024-01-17 10:48:33 -0300

    challenge completed 1.0

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-const plusIcons = document.querySelectorAll(".plus-icon");
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtener todos los iconos plus y minus
+  const plusIcons = document.querySelectorAll(".question-container__s2-details-icon#plus");
+  const minusIcons = document.querySelectorAll(".question-container__s2-details-icon#minus");
 
-plusIcons.forEach(function (img) {
-  img.addEventListener("click", function () {
-    if (img.src.endsWith("icon-plus.svg")) {
-      img.src = "./assets/images/icon-minus.svg";
-      img.alt = "icon minus";
-    } else {
-      img.src = "./assets/images/icon-plus.svg";
-      img.alt = "icon plus";
-    }
+  // Agregar un event listener a cada icono plus
+  plusIcons.forEach(function (plusIcon) {
+    plusIcon.addEventListener("click", function () {
+      // Encontrar el índice correspondiente al icono plus clickeado
+      const index = Array.from(plusIcons).indexOf(plusIcon);
+
+      // Ocultar el icono plus clicado y mostrar el icono minus correspondiente
+      plusIcon.style.display = "none";
+      minusIcons[index].style.display = "block";
+    });
+  });
+
+  // Agregar un event listener a cada icono minus
+  minusIcons.forEach(function (minusIcon) {
+    minusIcon.addEventListener("click", function () {
+      // Encontrar el índice correspondiente al icono minus clickeado
+      const index = Array.from(minusIcons).indexOf(minusIcon);
+
+      // Ocultar el icono minus clickeado y mostrar el icono plus correspondiente
+      minusIcon.style.display = "none";
+      plusIcons[index].style.display = "block";
+    });
   });
 });
+
+
+
+
